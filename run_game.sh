e7da93f962027b2aef92100c1f699caaf349b1f9
rapidFOX152 <dipra.sh152@gmail.com>
2026-03-15 07:35:07 +0600

    fix collision shape rotation issue; remove old helper scripts

--- run_game.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Get the directory where this script is located
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# Run the game using the Python from the virtual environment
-"$DIR/venv~/bin/python" "$DIR/main.py"
